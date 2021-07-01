2906234193241e2d474e2e936d6920afff7c838e
Mark <mark3fullen@gmail.com>
2021-07-01 00:22:58 -0400

    commit tree 7f9f8793b62bcf2943f6347cc2dda9174b2be1db

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+    return drivers.filter(data => data.toLowerCase() == name.toLowerCase());
+}
+
+const fuzzyMatch = (drivers, name) => {
+    return drivers.filter(data => data.substring(0, name.length) == name);
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter(key => key.name === string)
+}
